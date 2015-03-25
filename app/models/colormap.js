c665644a72b993b13fb9b65ab23a8de49f787023
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-03-24 21:36:28 -0400

    small model change

--- app/models/colormap.js
+++ app/models/colormap.js
@@ -10,13 +10,15 @@ export default DS.Model.extend({
     return this.get('colors').split(/\s+/).length;
   }.property('colors'),
 
-  gradient_css: function(){
-    var g = tinygradient(
-      _.zip( this.get("colors").split(' '), this.get("color_indexes").split(' ') )
+  color_json: function(){
+    return _.zip( this.get("colors").split(' '), this.get("color_indexes").split(' ') )
       .map( function(a){
         return {"color": a[0], "pos": parseInt(a[1]) / 255.0};
-      })
-    );
+      });
+  }.property('colors', 'color_indexes'),
+
+  gradient_css: function(){
+    var g = tinygradient(this.get('color_json'));
     return 'background:' + g.css() + ';height:48px;width:100%;';
-  }.property('colors', 'color_indexes')
+  }.property('color_json')
 });
